ac45344e4e9c10b04ee7af4e68c145e18fd5fbb1
testcoder-ui <songsongsecondnewlife@gmail.com>
2025-06-24 18:35:42 +0800

    feat:  add batch inference

--- shscripts/inference_framepack.sh
+++ shscripts/inference_framepack.sh
@@ -1,7 +1,7 @@
 # ckpt='checkpoints/wan/Wan2.1-I2V-14B-480P/'
 ckpt="./hf_download/"
 config='configs/010_framepack/framepack_i2v_hunyuan.yaml'
-prompt_dir="/project/llmsvgen/yazhou/check-base/maintain/temp/VideoTuna-dev/test-i2v-480"
+prompt_dir="inputs/i2v/576x1024"
 savedir="results/i2v/framepack"
 
 python3 scripts/inference_new.py \
@@ -9,11 +9,6 @@ python3 scripts/inference_new.py \
     --config "$config" \
     --prompt_dir "$prompt_dir" \
     --savedir "$savedir" \
-    --height 480 \
-    --width 832 \
-    --frames 81 \
     --seed 44 \
-    --num_inference_steps 40 \
-    --time_shift 3.0 \
-    --enable_model_cpu_offload
+    --num_inference_steps 25
         
